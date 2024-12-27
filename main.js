628fa130cc3362bada1fb532ae28d602850df03f
tuyson <huynhphantuyson71@gmail.com>
2024-12-27 16:10:48 +0700

    add zoom camera by gsap

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { RGBELoader } from 'three/examples/jsm/Addons.js';
 import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/Addons.js';
-import { color } from 'three/tsl';
+import JEASINGS from 'jeasings';
 
 
 const scene = new THREE.Scene();
@@ -49,11 +49,15 @@ const cube2 = new THREE.Mesh(
 );
 cube2.userData.origionalColor = 0xffffff;
 cube2.userData.label = "Thông tin 2";
-cube2.position.set(-5, -10, -7);
+cube2.position.set(-5, -18, -20);
 cubes.push(cube2);
-cubes.forEach(cube => {
+
+cubes.forEach((cube, index) => {
 	scene.add(cube);
 })
+
+
+
 
 // CSS2DRenderer
 const labelRenderer = new CSS2DRenderer();
@@ -80,7 +84,7 @@ loader.load(
 	},
 	// called while loading is progressing
 	function ( xhr ) {
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		// console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 	},
 	// called when loading has errors
 	function ( error ) {
@@ -89,8 +93,28 @@ loader.load(
 );
 
 const raycaster = new THREE.Raycaster();
+window.addEventListener('dblclick', zoomCam)
+window.addEventListener('mousedown', onMouseDownGltf)
 window.addEventListener('mousedown', onMouseDown);
 window.addEventListener( 'mousemove', onMouseMove );
+
+
+
+function zoomCam( event ) {
+	event.preventDefault();
+	const coords = new THREE.Vector2();
+	coords.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	coords.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+	raycaster.setFromCamera(coords, camera);
+
+	const intersects = raycaster.intersectObjects(scene.children);
+	if(intersects.length > 0){
+		zoomAt(intersects[0].point, camera);
+	} else {
+		
+}}
+
+
 function onMouseMove( event ) {
 	event.preventDefault();
 	const coords = new THREE.Vector2();
@@ -110,10 +134,28 @@ function onMouseMove( event ) {
 			cube.material.color.set(cube.userData.origionalColor);
 		})
 }}
+function onMouseDownGltf( event ) {
+	event.preventDefault();
+	const coords = new THREE.Vector2();
+	coords.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	coords.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+	raycaster.setFromCamera(coords, camera);
+
+	const intersects = raycaster.intersectObjects(scene.children);
+	if(intersects.length > 0){
+		console.log(intersects);
+		const p = intersects[0].point;
+		console.log('Tọa độ:',p.x, p.y, p.z);
+	} else {
+		cubes.forEach(cube => {
+			cube.material.color.set(cube.userData.origionalColor);
+		})
+}}
+
 
 function onMouseDown( event ) {
 	event.preventDefault();
-	const coords = new THREE.Vector2();
+	const coords = new THREE.Vector3();
 	coords.x = ( event.clientX / window.innerWidth ) * 2 - 1;
 	coords.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 	raycaster.setFromCamera(coords, camera);
@@ -122,18 +164,15 @@ function onMouseDown( event ) {
 	if(intersects.length > 0){
 		cubes.forEach(cube => {
 			if(intersects[0].object === cube){
-				console.log(cube.userData.label);
+				// console.log(cube.userData.label);
 				showLabel(cube.userData.label);
 			}
-		})
+		});
+		// zoomAt(intersects[0].object, camera);
 	}
 }
 
-function showLabel(object){
-	const objectInfo = document.querySelector('.objectInfo');
-	objectInfo.innerHTML = object;
-	objectInfo.style.display === 'none' ? objectInfo.style.display = 'block' : objectInfo.style.display = 'none';
-}
+
 
 camera.position.z = 40;
 
@@ -152,3 +191,42 @@ window.addEventListener('resize', () => {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 
 })
+
+
+// Interact JS
+const tagContainer = document.querySelector('.tagContainer');
+const exitBtn = document.querySelector('.exitBtn');
+exitBtn.addEventListener('click', () => {
+	tagContainer.style.display = 'none';
+});
+
+function showLabel(object){
+	const contentContainer = document.querySelector('.contentContainer');
+	contentContainer.textContent = object;
+	tagContainer.style.display = 'flex';
+}
+
+// Zoome Gsap
+const zoomAt = (target, camera) => {
+
+	gsap.to( camera.position, {
+		duration: 1,
+		x: target.x,
+		y: target.y,
+		z: target.z + 20,
+		onUpdate: function() {
+			camera.lookAt(target.x, target.y, target.z); //important 
+		}
+	} );
+
+	gsap.to( controls.target, {
+		duration: 1,
+		x: target.x,
+		y: target.y, //set the center of the controler to the zoomed object 
+		z: target.z , // no distance needed
+		onComplete: ()=>{
+			controls.update();
+			controls.enabled = true; // activate the controler again after animation
+		}
+	} );
+};
