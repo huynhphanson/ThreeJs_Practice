4217718dab92b7696c016e976bbcc79a40944fa8
tuyson <huynhphantuyson71@gmail.com>
2025-04-15 16:30:08 +0700

    merge feature/ruler & cursor-coordinate

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ const { tilesRenderer, dispose } = load3dTilesModel(tilesPath, camera, renderer,
 
 // Load GLTF Model
 const gltfPath1 = '../../resources/models/glb/songChoSurfaceDra.glb';
-// loadGLTFModel(gltfPath1, scene, camera, controls, 'surface');
+loadGLTFModel(gltfPath1, scene, camera, controls, 'surface');
 const gltfPath2 = '../../resources/models/glb/songChoBlueDra.glb';
 loadGLTFModel(gltfPath2, scene, camera, controls, 'buildings');
 
